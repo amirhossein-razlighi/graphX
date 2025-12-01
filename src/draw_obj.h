a2ffc9d3be14b0e81a48d3607d7758e8032fb8e6
AmirHossein-nr <arazlighi@gmail.com>
2025-12-02 00:21:14 +0330

    now we can draw a wireframe of an obj file!

---
+++ src/draw_obj.h
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "tgaimage.h"
+
+void drawWireframe(const std::string &filename, TGAImage &frameBuffer, TGAColor lineColor);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "tgaimage.h"
 #include "line.h"
+#include "draw_obj.h"
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -7,23 +8,14 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+const std::string filename = "./diablo3_pose.obj";
+
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
+    constexpr int width  = 512;
+    constexpr int height = 512;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
-
-    line(ax, ay, bx, by, framebuffer, blue);
-    line(cx, cy, bx, by, framebuffer, green);
-    line(cx, cy, ax, ay, framebuffer, yellow);
-    line(ax, ay, cx, cy, framebuffer, red);
-
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    drawWireframe(filename, framebuffer, white);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
