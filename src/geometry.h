777267c18ac7e1989e969899147b390f8c1c5e0c
AmirHossein-nr <arazlighi@gmail.com>
2025-12-18 22:08:01 +0330

    add support for mmore operations on vecotrs

--- src/geometry.h
+++ src/geometry.h
@@ -17,6 +17,45 @@ struct vec
     assert(i >= 0 && i < n);
     return data[i];
   }
+
+  double operator*(const vec<n> &otherVec) const {
+    double dotProduct = 0.0;
+    for (int i = 0; i < n; i++) {
+      dotProduct += this->data[i] * otherVec.data[i];
+    }
+    return dotProduct;
+  }
+
+  vec<n> operator*(const double scalar) const {
+    vec<n> result;
+    for (int i = 0; i < n; i++) {
+      result.data[i] = this->data[i] * scalar;
+    }
+    return result;
+  }
+
+  double l2norm() const {
+    return (this * this);
+  }
+
+  double norm() const {
+    return std::sqrt(l2norm());
+  }
+  vec<n> operator+(const vec<n> &otherVec) const {
+    vec<n> result;
+    for (int i = 0; i < n; i++) {
+      result.data[i] = this->data[i] + otherVec.data[i];
+    }
+    return result; 
+  }
+  vec<n> operator-(const vec<n> &otherVec) const {
+    vec<n> result;
+    for (int i = 0; i < n; i++) {
+      result.data[i] = this->data[i] - otherVec.data[i];
+    }
+    return result;
+  }
+
 };
 
 template <int n>
