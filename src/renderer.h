987e3f228e4a31e02a209064f4cdf965f4984398
AmirHossein-nr <arazlighi@gmail.com>
2025-12-19 23:50:28 +0330

    renderer takes zBuffer as vector<double>

--- src/renderer.h
+++ src/renderer.h
@@ -6,11 +6,12 @@ class Renderer
 {
 public:
     TGAImage &framebuffer;
-    TGAImage &zbuffer;
-    Renderer(TGAImage &fb, TGAImage &zb) : framebuffer(fb), zbuffer(zb) {}
+    std::vector<double> &zbuffer;
+    Renderer(TGAImage &fb, std::vector<double> &zb) : framebuffer(fb), zbuffer(zb) {}
     void drawLine(int x0, int y0, int x1, int y1, TGAColor color);
     void drawTriangle(int x0, int y0, int x1, int y1, int x2, int y2, TGAColor color);\
     void drawFilledTriangle(const Triangle &tri);
     void drawFilledTriangleLegacy(int x0, int y0, int x1, int y1, int x2, int y2, TGAColor color);
     void drawBoundingBox(const Triangle &tri, TGAColor color);
+    void renderZBuffer(const std::string &filename);
 };
